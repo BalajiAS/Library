27eb805d4cab3d514192495c7ac717dc08cd3e4e
sslap <sslap>
2015-12-27 23:38:36 +0530

    story1:balaji_as:First Commit testing the CI

---
+++ src/main/java/com/library/inventory/LibraryInventory.java
@@ -0,0 +1,12 @@
+package com.library.inventory;
+
+/**
+ * Created by sslap on 27-12-2015.
+ */
+public class LibraryInventory {
+
+    public static void main (String a[]) {
+        System.out.println("Test");
+        
+    }
+}
